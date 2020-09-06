b709738c002b1fbdb0c74e6a6e7cda108285f90d
gomezbcn <jballaringomez@gmail.com>
2020-09-06 21:32:19 +0200

    lpp

--- MODULO-1/js/demo.js
+++ MODULO-1/js/demo.js
@@ -1,7 +1,4 @@
-
-
 document.getElementById("name").value = "Mi nombre";
 document.getElementById("lastName").value = "Mi apellido";
-
-document.getElementById("avatar123").src = "img/avatar123.png";
-
+// diff
+document.getElementById("avatar").src = "img/avatar123.png";
