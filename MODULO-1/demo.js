e4f840175f2ba63a59bb07ec66c17177886b98d1
gomezbcn <jballaringomez@gmail.com>
2020-09-06 21:22:08 +0200

    molmo

---
+++ MODULO-1/demo.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Mi apellido";
+
+document.getElementById("avatar123").src = "img/avatar123.png";
+
